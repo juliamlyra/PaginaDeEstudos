df6a326d1a7c36b86ab00ce014cea5a02b129118
Julia Mendonça <julialyra7@gmail.com>
2022-07-07 11:45:00 -0300

    melhoria controller.js

--- controller.js
+++ controller.js
@@ -1,27 +1,14 @@
 //Objetivo: Ao clicar no botão 'Resultado', capturar os valores dos inputs, fazer a soma e exibir na div com id 'Resultado'.
-let inputvalue1;
-let inputvalue2;
 
-let input1;
-let input2;
-let exibirResultado;
-
-let resultadoSoma;
-
-function somaDeDoisNumeros(num1, num2) {
-  let resultado = num1 + num2;
+function calc(valor1, valor2) {
+  let resultado = valor1 + valor2;
   return resultado;
 }
 
-function onClickResult() {
-  input1 = document.getElementById("num1");
-  input2 = document.getElementById("num2");
-  exibirResultado = document.getElementById("resultado");
 
-  inputvalue1 = parseFloat(input1.value);
-  inputvalue2 = parseFloat(input2.value);
-
-  resultadoSoma = somaDeDoisNumeros(inputvalue1, inputvalue2);
-
-  exibirResultado.textContent = resultadoSoma;
+function onClickGetResult() {
+  let inputValue1 = parseFloat(document.getElementById("valor1").value);
+  let inputValue2 = parseFloat(document.getElementById("valor2").value);
+  let exibirResultado = document.getElementById("resultado");
+  exibirResultado.textContent = calc(inputValue1, inputValue2);
 }
